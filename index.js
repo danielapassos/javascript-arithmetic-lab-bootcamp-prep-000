94b014ceb64027e607aec7b74a050f69f67d37da
Daniela Passos <moitidani@gmail.com>
2018-06-28 17:54:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
 var a,b
 
 function add(a,b){
-  a+=b;
-  console.log(a)
+  return a + b
 }
 
 function subtract(a,b){
-  a-=b;
-  console.log(a)
+  return a - b
 }
 
 function multiply(a,b){
-  a*=b;
-  console.log(a)
+  return a * b
 }
 
 function divide(a,b){
-  a/=b;
-  console.log(a)
+  return a / b
 }
 
 function inc(a){
